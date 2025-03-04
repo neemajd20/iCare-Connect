fab7406127105347ff435cd0c437b500e0e68606
neemajd20 <neemajd20@gmail.com>
2025-03-04 11:25:21 +0300

    add:,src/index.js,styles.module.css

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,48 +1,36 @@
+import React from 'react';
 import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
+// Define the type for FeatureItem
 const FeatureList = [
-  
   {
     title: 'iCare-Student',
-    Svg: require('@site/static/img/UDSM-logo.PNG.svg').default,
+    Svg: require('@site/static/img/UDSM-logo.PNG.svg').default, // Ensure correct import
     description: (
-      <>
-        
-      </>
+      <p>A platform dedicated to student welfare and academic management.</p>
     ),
   },
- 
-  
 ];
 
-function Feature({Svg, title, description}) {
+// Feature component
+function Feature({ title, Svg, description }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={clsx(styles.feature)}>
+      <Svg className={styles.featureSvg} role="img" />
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>{description}</p>
     </div>
   );
 }
 
-
-
+// Homepage Features component
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
+    <section className={clsx(styles.features)}>
+      {FeatureList.map((feature, idx) => (
+        <Feature key={idx} {...feature} />
+      ))}
     </section>
   );
 }
